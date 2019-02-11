24b8b287a3555283ceb3869672bcae4d3d2866b9
Huseyin <hbalcayev@gmail.com>
2019-02-11 14:41:01 -0600

    Login Page Created

---
+++ src/test/java/com/cybertek/pages/BriteLoginPage.java
@@ -0,0 +1,24 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.openqa.selenium.By.xpath;
+
+public class BriteLoginPage {
+    WebDriver driver;
+
+    public BriteLoginPage(){
+        driver = Driver.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath="//*[@id='login']")
+    public WebElement loginBox;
+
+    @FindBy(xpath="//*[@id='password']")
+    public WebElement passwordBox;
+}
---
+++ src/test/resources/features/Brite.feature
@@ -0,0 +1,7 @@
+@brite
+Feature: Kanban Test2
+
+  Scenario: See listings as List
+    When user goes to Brite application
+    Then user should see login page
+    And
